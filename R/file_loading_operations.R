7adf4dab08921e568973e9f1f99040a8dfc00cf3
Balint Szebenyi <szebenyib@gmail.com>
2018-04-24 09:52:53 +0200

    Adding load_csv_tab_separated for data exported from ztabdl

--- R/file_loading_operations.R
+++ R/file_loading_operations.R
@@ -46,6 +46,41 @@ load_csv_local_unconverted <- function(filename) {
   colnames(df) <- strip_x_bic(colnames(df))
   return(df)
 }
+
+# Reads a csv file that has been saved from a SAP table
+# as a "local file" with the "unconverted" setting.
+#
+# In this case the file is full of | chars and there is
+# a header like that at the top of the file.
+#
+# @param filename the name of the file with extension
+# @return tbl_df
+# @examples
+# collection <- NULL
+# collect_chars_from_string("abc", collection)
+load_csv_tab_separated <- function(filename) {
+  if (!exists("RBW_INPUT_FILES_DIR")) {
+    stop("RBW_INPUT_FILES_DIR must be set before using this function, call init_file_loading")
+  }
+  if (exists("RBW_SYSTEM_LANDSCAPE") && !is.na(RBW_SYSTEM_LANDSCAPE)) {
+    filename <- paste0(RBW_SYSTEM_LANDSCAPE,
+                       "_",
+                       filename)
+  }
+  df <- read.csv(file = file.path(RBW_INPUT_FILES_DIR,
+                                  filename),
+                 sep = "\t",
+                 dec = ",",
+                 na.strings = "",
+                 stringsAsFactors = FALSE)
+  df <- tbl_df(df)
+  # Removing unnecessarily created rows and columns
+  # df <- df[3:nrow(df)-1, 3:ncol(df)-1]
+  # Cleaning column names
+  colnames(df) <- strip_x_bic(colnames(df))
+  return(df)
+}
+
 # Collects special characters uniquely from a vector
 #
 # This can be used to observe which (special) characters
--- tests/testthat/test_file_loading_operations.R
+++ tests/testthat/test_file_loading_operations.R
@@ -31,3 +31,26 @@ test_that("load_csv_local_unconverted", {
   rm("RBW_INPUT_FILES_DIR", "RBW_SYSTEM_LANDSCAPE",
      pos = ".GlobalEnv")
 })
+
+test_that("load_csv_tab_separated", {
+  expect_false(exists("RBW_INPUT_FILES_DIR"))
+  expect_error(load_csv_tab_separated(
+    filename = "tests/testdata/sap_bic_agh_mm_1000_all_tab.txt"),
+    "RBW_INPUT_FILES_DIR must be set before using this function, call init_file_loading")
+
+  RBW_INPUT_FILES_DIR <<- file.path("..", "testdata")
+  df <- load_csv_tab_separated(filename = "bic_agh_mm_1000_all_tab.txt")
+  expect_equal(nrow(df), 5)
+  expect_equal(ncol(df), 13)
+  expect_false(any(grepl("BIC", colnames(df))),
+               "Column names contain BIC, they were not cleaned")
+
+  RBW_SYSTEM_LANDSCAPE <<- "sap"
+  df <- load_csv_tab_separated(filename = "bic_agh_mm_1000_all_tab.txt")
+  expect_equal(nrow(df), 3)
+  expect_equal(ncol(df), 13)
+  expect_false(any(grepl("BIC", colnames(df))),
+               "Column names contain BIC, they were not cleaned")
+  rm("RBW_INPUT_FILES_DIR", "RBW_SYSTEM_LANDSCAPE",
+     pos = ".GlobalEnv")
+})
