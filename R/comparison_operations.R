b0dc49aa9ebca51dacf76c9687e46b3bc0a93bfc
Balint Szebenyi <szebenyib@gmail.com>
2018-04-24 11:23:43 +0200

    Adding column name comparison

---
+++ R/comparison_operations.R
@@ -0,0 +1,39 @@
+# Compares whether the two data frames have the same column names and
+# in the same order
+#
+# @param x the first dataframe
+# @param y the second dataframe
+# @param x_name optional name of the first dataframe, used for messages
+# @param y_name optional name of the second dataframe, used for messages
+# @return TRUE if the dataframes have the same column names
+# @examples
+# compare_same_colnames(x = x,
+#                       y = y,
+#                       x_name = "first",
+#                       y_name = "second")
+compare_same_colnames <- function(x, y, x_name = "x", y_name = "y") {
+  x_colnames <- colnames(x)
+  y_colnames <- colnames(y)
+  if (length(x_colnames) != length(y_colnames)) {
+    stop(paste0(x_name, " and ", y_name,
+        " do not contain the same number of columns, ",
+        x_name, " has ", length(x_colnames), ", while ",
+        y_name, " has ", length(y_colnames)))
+  }
+  if (!all(x_colnames %in% y_colnames)) {
+    stop(paste0(y_name, " did not contain: ",
+                paste(x_colnames[!x_colnames %in% y_colnames],
+                      collapse = " ")))
+  }
+  if (!all(y_colnames %in% x_colnames)) {
+    stop(paste0(x_name, " did not contain: ",
+                paste(y_colnames[!y_colnames %in% x_colnames],
+                      collapse = " ")))
+  }
+  if (all(x_colnames == y_colnames)) {
+    result <- TRUE
+  } else {
+    stop(paste0(x_name, " and ", y_name,
+                " have their columns in different order"))
+  }
+}
---
+++ tests/testthat/test_comparison_operations.R
@@ -0,0 +1,65 @@
+context("Comparison operations")
+
+test_that("compare_same_colnames", {
+  x <- mtcars
+  y <- mtcars[, 1:ncol(mtcars) - 1]
+  expect_error(compare_same_colnames(x = x, y = y,
+                                     x_name = "STD", y_name = "LAST_MISSING"),
+               paste0("STD and LAST_MISSING do not contain the same number",
+                      " of columns, STD has 11, while LAST_MISSING has 10"))
+
+  # Changing order
+  x <- mtcars
+  y <- cbind(mtcars[, ncol(mtcars)],
+             mtcars[, 1:ncol(mtcars) - 1])
+  x_names <- colnames(x)
+  colnames(y) <- c(
+    x_names[length(x_names)],
+    x_names[1:length(x_names) - 1])
+  expect_error(compare_same_colnames(x = x, y = y,
+                                     x_name = "STD", y_name = "CHANGED_ORDER"),
+               "STD and CHANGED_ORDER have their columns in different order")
+
+  # Different column names
+  x <- mtcars
+  y <- mtcars
+  x_names <- colnames(x)
+  colnames(y) <- c(
+    "foo",
+    x_names[1:length(x_names) - 1])
+  expect_error(compare_same_colnames(x = x, y = y,
+                                     x_name = "STD", y_name = "ALT"),
+               "ALT did not contain: carb")
+
+  x <- mtcars
+  y <- mtcars
+  x_names <- colnames(x)
+  colnames(x) <- c(
+    "foo", "bar",
+    x_names[3:length(x_names) - 1])
+  expect_error(compare_same_colnames(x = x, y = y,
+                                     x_name = "STD", y_name = "ALT"),
+               "ALT did not contain: foo bar")
+
+  # Identical
+  x <- mtcars
+  y <- mtcars
+  expect_true(compare_same_colnames(x = x, y = y,
+                                    x_name = "STD", y_name = "CLONE"))
+})
+
+test_that("compare_same_coltypes", {
+  expect_true(TRUE)
+})
+
+test_that("compare_same_rowcount", {
+  expect_true(TRUE)
+})
+
+test_that("compare_same_structure", {
+  expect_true(TRUE)
+})
+
+test_that("compare_same_content", {
+  expect_true(TRUE)
+})
